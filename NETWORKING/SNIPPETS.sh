4e10eecd7add83b85c407b416f29d370c2ceff5a
William Martens <68499986+loneicewolf@users.noreply.github.com>
2023-08-01 16:03:03 +0200

    Update SNIPPETS.sh

--- NETWORKING/SNIPPETS.sh
+++ NETWORKING/SNIPPETS.sh
@@ -1,9 +1,13 @@
 # Snippets & Commands
 
-# Extract http objects to DIR TSHARK_EXP_http/
-pcap="traffic.pcap" ; EXP_PROTO='http' ; tshark -r $pcap --export-objects "$EXP_PROTO,TSHARK_EXP_""$EXP_PROTO"
-# Extract imf objects to DIR TSHARK_EXP_imf/
-pcap="traffic.pcap" ; EXP_PROTO='imf' ; tshark -r $pcap --export-objects "$EXP_PROTO,TSHARK_EXP_""$EXP_PROTO"
+
+# ------------------------------------------------------------
+## TSHARK
+# note: you can use -q,-n
+
+# Extract http,imf objs to DIR TSHARK_EXP_http/ , DIR TSHARK_EXP_imf/  Respectively
+pcap="traffic.pcap" ; EXP_PROTO='http' ; tshark  -q  -r $pcap --export-objects "$EXP_PROTO,TSHARK_EXP_""$EXP_PROTO"
+pcap="traffic.pcap" ; EXP_PROTO='imf' ; tshark  -q  -r $pcap --export-objects "$EXP_PROTO,TSHARK_EXP_""$EXP_PROTO"
 
 # Grab MAC Addresses
 pcap="traffic.pcap" ; tshark -q -n -r $pcap -z endpoints,eth
@@ -17,3 +21,12 @@ pcap="traffic.pcap" ; tshark -q -n -r $pcap -z expert
 # Others
 pcap="traffic.pcap" ; tshark -q -n -r $pcap -z http_srv,tree
 pcap="traffic.pcap" ; tshark -q -n -r $pcap -z http_req,tree
+
+# ------------------------------------------------------------
+
+
+
+
+
+
+
