de4c882f88b6d6c61dbc56432c10599bf38bf60e
William Martens <68499986+loneicewolf@users.noreply.github.com>
2023-08-03 13:20:39 +0200

    Update SNIPPETS.sh

--- NETWORKING/SNIPPETS.sh
+++ NETWORKING/SNIPPETS.sh
@@ -14,6 +14,11 @@ pcap="traffic.pcap" ; EXP_PROTO='imf'  ; tshark  -q  -r $pcap --export-objects "
 # same but for loop. Note there can be many variations for this.
 ## pcap="traffic.pcap" ; for p in {"http","imf"}; do EXP_PROTO="$p" ; tshark -q -r $pcap --export-objects "$EXP_PROTO,TSHARK_EXP_""$EXP_PROTO"; done
 
+# make a file manual_protocols.txt which has http and imf at 2 sep.lines
+# and mkdir's : tools/tshark/export_obj  <- this is where http and imf dirs will be created.
+pcap="traffic.pcap"; f="manual_protocols.txt";i=0;while read L; do ((i=i+1)); p="$L" ; tshark -q -r $pcap --export-objects "$p,tools/tshark/export_obj/$p" ; done < $f
+
+
 # Grab MAC Addresses
 pcap="traffic.pcap" ; tshark -q -n -r $pcap -z endpoints,eth | grep -oE '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | sort --unique
 
